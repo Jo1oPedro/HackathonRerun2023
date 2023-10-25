688f0d41a1fd9fc33e195056cc68d276ded79f9e
GabrielGomesBahia <gabrielgbahia@gmail.com>
2023-10-24 22:08:41 -0300

    Transformando output do regex em objetos

--- parser/parser.php
+++ parser/parser.php
@@ -1,13 +1,12 @@
 <?php
-$lines = file('./arquivosDeEntrada/arquivoDeEntrada1.txt', FILE_IGNORE_NEW_LINES);
+$lines = file('./arquivosDeEntrada/arquivoDeEntrada2.txt', FILE_IGNORE_NEW_LINES);
 
 $classes = array();
 $structs = array();
 $insideClassOrStruct = false;
+$extendsAux = false;
 
 foreach ($lines as $line) {
-    $objeto = new stdClass();
-    $extendsAux = false;
     $words = str_word_count($line, 1);
     foreach ($words as $key => $word) {
         if($word === "extends") {
@@ -18,136 +17,55 @@ foreach ($lines as $line) {
     if (preg_match('/^(struct|class|Class) (\w+)/', $line, $matches)) {
         $type = $matches[1];
         $name = $matches[2];
-        if($name) {
-            $objeto->name = $name;
-        }
+        $objectName = $name;
         if ($type === 'struct') {
-            $objeto->type = 'struct';
+            $objectType = 'struct';
             $insideClassOrStruct = true;
         } elseif ($type === 'class' || $type === 'Class') {
-            $objeto->type = 'class';
+            $objectType = 'class';
             $insideClassOrStruct = true;
         }
     } elseif ($insideClassOrStruct) {
-        if (strpos($line, '{') !== false) {
-            $attributes = array();
-        } elseif (strpos($line, '}') !== false) {
-            $insideClassOrStruct = false;
+        if (strpos($line, '}') !== false) {
+            $object = new stdClass();
+            $object->name = $objectName;
+            $object->type = $objectType;
+            $object->extends = $extendsAux;
             $atributosObj = [];
             foreach($attributes as $attr) {
                 $words = str_word_count($attr, 1);
                 $atributo = new stdClass();
-                $atributo->type = $words[0];
-                $atributo->value = $words[1];
+                if(count($words) === 4) {
+                    $atributo->struct = true;
+                    $atributo->type = $words[2];
+                    $atributo->value = $words[3];
+                }else if(count($words) === 2) {
+                    $atributo->struct = false;
+                    $atributo->type = $words[0];
+                    $atributo->value = $words[1];
+                }else {
+                    $atributo->struct = false;
+                    $atributo->type = $words[1];
+                    $atributo->value = $words[2];
+                }
                 $atributosObj[] = $atributo;
             }
-            $objeto->atributos = $atributosObj;
+            $object->atributos = $atributosObj;
+            if(isset($object->type)) {
+                if($object->type === 'struct') {
+                    $structs[] = $object;
+                }else if($object->type === 'class') {
+                    $classes[] = $object;
+                }
+            }
             $attributes = array();
-        } else {
+            $extendsAux = false;
+            $insideClassOrStruct = false;
+        } else if(strpos($line, '{') === false){
             $attributeLine = trim($line, " \t;");
             if (!empty($attributeLine)) {
                 $attributes[] = $attributeLine;
             }
         }
     }
-
-    if(isset($objeto->type)) {
-        if($objeto->type === 'struct') {
-            $structs[] = $objeto;
-        }else if($objeto->type === 'class') {
-            $classes[] = $objeto;
-        }
-    }
 }
-
-var_dump($classes);exit;
-
-
-// $lines = file('./arquivosDeEntrada/arquivoDeEntrada2.txt', FILE_IGNORE_NEW_LINES);
-
-// $structs = array();
-// $classesLowercase = array();
-// $classesUppercase = array();
-
-// $classExtends = array();
-// $classUpExtends = array();
-// $classData = array();
-// $structData = array();
-// $currentClass = null;
-// $currentStruct = null;
-// $insideClassOrStruct = false;
-// $attributes = array();
-
-// foreach ($lines as $line) {
-//     $extendsAux = false;
-//     $words = str_word_count($line, 1);
-//     foreach ($words as $key => $word) {
-//         if($word === "extends") {
-//             $extendsAux = $words[$key + 1];
-//             break;
-//         }
-//     }
-//     if (preg_match('/^(struct|class|Class) (\w+)/', $line, $matches)) {
-//         $type = $matches[1];
-//         $name = $matches[2];
-
-//         if ($type === 'struct') {
-//             $structs[] = $name;
-//             $currentStruct = $name;
-//             $insideClassOrStruct = true;
-//         } elseif ($type === 'class') {
-//             $classesLowercase[] = $name;
-//             $currentClass = $name;
-//             $insideClassOrStruct = true;
-//             if($extendsAux) {
-//                 $classExtends[$currentClass] = $extendsAux;
-//             }
-//         } elseif ($type === 'Class') {
-//             $classesUppercase[] = $name;
-//             $currentClass = $name;
-//             $insideClassOrStruct = true;
-//             if($extendsAux) {
-//                 $classUpExtends[$currentClass] = $extendsAux;
-//             }
-//         }
-//     } elseif ($insideClassOrStruct) {
-//         if (strpos($line, '{') !== false) {
-//             $attributes = array();
-//         } elseif (strpos($line, '}') !== false) {
-//             $insideClassOrStruct = false;
-//             if ($currentClass) {
-//                 $classData[$currentClass] = array('attributes' => $attributes);
-//             } elseif ($currentStruct) {
-//                 $structData[$currentStruct] = array('attributes' => $attributes);
-//             }
-//             $attributes = array();
-//         } else {
-//             $attributeLine = trim($line, " \t;");
-//             if (!empty($attributeLine)) {
-//                 $attributes[] = $attributeLine;
-//             }
-//         }
-//     }
-// }
-
-// echo "Structs: " . implode(', ', $structs) . PHP_EOL;
-// echo "Classes (Iniciando com letra minúscula): " . implode(', ', $classesLowercase) . PHP_EOL;
-// echo "Classes (Iniciando com letra maiúscula): " . implode(', ', $classesUppercase) . PHP_EOL;
-
-// foreach ($classData as $className => $data) {
-//     echo "Atributos da classe $className: " . implode(', ', $data['attributes']) . PHP_EOL;
-// }
-
-// foreach ($structData as $structName => $data) {
-//     echo "Atributos da struct $structName: " . implode(', ', $data['attributes']) . PHP_EOL;
-// }
-
-// foreach ($classExtends as $className => $value) {
-//     echo "Extends da $className: " . $value . PHP_EOL;
-// }
-
-// foreach ($classUpExtends as $className => $value) {
-//     echo "Extends da $className: " . $value . PHP_EOL;
-// }
-
-
